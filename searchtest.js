888524d12ffa130ad93c049dfa8be6c1d3b5640b
LauraBrisbois <lbrisbois8@gmail.com>
2019-01-16 23:15:34 -0500

    Commiting searchtest

--- searchtest.js
+++ searchtest.js
@@ -1,10 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var lastid = null;
+var lastlength;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    var lastid = null;
+
     client.guilds.tap((guild) => {
+        while(lastlength != 0)
+        searchguild(guild, lastid)
+
+    });
+});
+function searchguild(guild, lastid = null)
+        {
         guild.search({
             author: '163471920359145472',
             contextSize: '0',
@@ -12,19 +20,20 @@ client.on('ready', () => {
         })
             .then((response) => {
                 //lastid = response.messages[0][0].createdAt;
-                console.log(response.messages[0]); 
+                //console.log(response.messages[0]); 
+                const lastid = response.messages[response.messages.length-1][0].createdAt;
+                //console.log(lastid)
+                lastlength = response.messages.length;
                 response.messages.forEach((array) => {
-                    //console.log(response.messages);
                     array.forEach((message) => {
+
                     //console.log(message.content);
-                    //console.log(response.message);
-                    //console.log(guild.id);
+                    
                     
                     });
                 });
+                 searchguild(guild)
             });
-
-    });
-});
-
+            
+        }
 client.login('MTM3MDM4OTE3MjY1MzkxNjE2.DmiVOw.Pm2tOTQW6AdMiyl5eDcpfVI538c');
