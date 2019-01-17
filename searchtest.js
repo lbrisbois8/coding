1c39d41979cc4b0927084cdb0c53a65bdb900021
LauraBrisbois <lbrisbois8@gmail.com>
2019-01-16 23:57:51 -0500

    config .env shit

--- searchtest.js
+++ searchtest.js
@@ -1,13 +1,12 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var lastid = null;
-var lastlength;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
     client.guilds.tap((guild) => {
-        while(lastlength != 0)
-        searchguild(guild, lastid)
+        searchguild(guild)
 
     });
 });
@@ -23,17 +22,16 @@ function searchguild(guild, lastid = null)
                 //console.log(response.messages[0]); 
                 const lastid = response.messages[response.messages.length-1][0].createdAt;
                 //console.log(lastid)
-                lastlength = response.messages.length;
                 response.messages.forEach((array) => {
                     array.forEach((message) => {
 
-                    //console.log(message.content);
+                    console.log(message.content);
                     
                     
                     });
                 });
-                 searchguild(guild)
+                if(response.messages.length != 0) searchguild(guild)
             });
             
         }
-client.login('MTM3MDM4OTE3MjY1MzkxNjE2.DmiVOw.Pm2tOTQW6AdMiyl5eDcpfVI538c');
+client.login(process.env.TOKEN);
